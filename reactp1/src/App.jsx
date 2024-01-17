59724335622533baa8299051a17ff0c75029ed6b
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2024-01-17 15:39:40 +0530

    first commit

---
+++ reactp1/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+function App() {
+  //let counter=15;
+  let [counter,setCounter]=useState(15)
+  const addValue=()=>{
+    console.log(`clicked :`,counter)
+    //counter=counter+1
+    setCounter(counter+1)
+  }
+  const removeValue=()=>{
+    if(counter==0){
+       setCounter(0);
+    }else{
+      setCounter(counter-1)
+    }
+  }
+  return (
+    <>
+    <h1>hello from reactp1</h1>
+    <h2>counter value :{counter}</h2>
+    <button onClick={addValue}>Add value : {counter}</button><br/>
+    <button onClick={removeValue}>Remove value : {counter}</button>
+    </> 
+  )
+}
+
+export default App
