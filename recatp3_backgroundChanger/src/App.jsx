6ec6f7231f1fd116e7ad52ce92d8c7ba0ed29890
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2024-02-07 09:15:09 +0000

    third commit

--- recatp3_backgroundChanger/src/App.jsx
+++ recatp3_backgroundChanger/src/App.jsx
@@ -4,8 +4,8 @@ function App() {
 const [color,setColor] = useState("olive")
   return (
 <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
-  <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-    <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+  <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0">
+    <div className="flex flex-wrap justify-center gap-3 bg-white px-3 py-2 rounded-3xl">
       <button
       onClick={ ()=> setColor("red")}
       className="outline-none bg-red-500 text-white rounded-full px-4 py-1 shadow-lg">red</button>
