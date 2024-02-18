70f2b94676a097165119a75829234952b175c3d4
Premkumar13m <119662418+Premkumar13m@users.noreply.github.com>
2024-02-18 18:39:37 +0000

    first commit

---
+++ codeStepByStep/31_ActiveLink/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div>
+            <center>
+            <h1> this is the Home Page</h1>
+            </center>
+        </div>
+    )
+}
+export default Home;
